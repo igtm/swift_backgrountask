cdbeec24dd2b3029b39df855b664fb3548159bea
Iguchi Tomokatsu <iguchi.tomokatsu@designone.jp>
2018-12-19 10:53:42 +0900

    Initial Commit

---
+++ BackgroundTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BackgroundTask
+//
+//  Created by 井口 智勝 on 2018/12/19.
+//  Copyright © 2018年 井口 智勝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
